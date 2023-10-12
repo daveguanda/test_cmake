b8719c48203c15d2b54c9826962f59d6acd2dc9c
itdagua1 <davide.guandalini@toyota-industries.eu>
2023-10-12 14:50:06 +0200

    init repository

---
+++ build/src/CMakeFiles/simplecmake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simplecmake.dir/printhello.cpp.obj"
+  "CMakeFiles/simplecmake.dir/printhello.cpp.obj.d"
+  "libsimplecmake.a"
+  "libsimplecmake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simplecmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ example/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "../include/simplecmake/printhello.h"
+
+int main()
+{
+    //std::cout << "ciao\n";
+    printhello();
+    //return 0;
+}
